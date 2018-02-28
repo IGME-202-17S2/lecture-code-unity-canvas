e33d9903de7e103464e64c52024406164a20db38
Percipient24 <jcpigm@rit.edu>
2018-02-28 00:38:22 -0500

    Blank Starter Project

---
+++ Assets/Scripts/DemoScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DoAThing() {
+		Debug.Log ("I did a thing!");
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lecture-code-unity-canvas
 Sample project demonstrating Unity UI Canvas
+
+This repo started with the base project. (it doesn't do much)
+At the end of Thursday's class, I will push updates.
+
+Sprite Assets from https://ravenmore.itch.io/2d-pickups-pack
